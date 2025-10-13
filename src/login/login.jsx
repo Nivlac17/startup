9a6bbd843328139fa393814f76dc5391ad7d6a82
Calvin Merrell <calvinm7@byu.edu>
2025-10-13 17:16:36 -0600

    login finnished

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 
+
 export function Login() {
 return (
      <main className="container-fluid ">
       <div>
-        log-in desplayed here
-      </div>
+              <h1>Lines of Light</h1>
+
+      <form method="get" to="navigation.html">
+        <div>
+          {/* <!-- <span>@</span> --> */}
+          <input type="text" placeholder="your@email.com" />
+        </div>
+        <div>
+          {/* <!-- <span>🔒</span> --> */}
+          <input type="password" placeholder="password" />
+        </div>
+        <section>
+          <button type="submit">Login</button>
+          <button type="submit">Register</button>
+        </section>
+      </form>
+            </div>
     </main>
 );
 }
