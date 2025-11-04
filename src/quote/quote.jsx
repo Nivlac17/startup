3f9fcc15a2f5ccb6f5fb6d5cd0019d87b63c8508
Calvin Merrell <calvinm7@byu.edu>
2025-11-04 11:08:23 -0700

    quote page updated correctly

--- src/quote/quote.jsx
+++ src/quote/quote.jsx
@@ -3,13 +3,22 @@ import './quote.css';
 
 
 export function Quote() {
+
+  const [quote, setQuote] = React.useState('Loading...');
+  const [quoteAuthor, setQuoteAuthor] = React.useState('unknown');
+
+    React.useEffect(() => {
+      setQuote('Veni, Vidi, Vici');
+      setQuoteAuthor('Julius');
+    }, []);
+
 return (
      <main className="container-fluid ">
       <div>
       <h1>Here is our Inspirational Quote!</h1>
       <div id="quote" className="quote-box bg-light text-dark">
-          <p className="quote">Veni, Vidi, Vici</p>
-          <p className="author">Julius</p>
+          <p className='quote'>{quote}</p>
+          <p className='author'>{quoteAuthor}</p>
         </div>
     </div>
     </main>
