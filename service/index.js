59ef931b73729d03df99bf3d44c45f3e6d6b56a2
Calvin Merrell <calvinm7@byu.edu>
2025-11-11 16:39:24 -0700

    index.js updated

--- service/index.js
+++ service/index.js
@@ -8,19 +8,13 @@ const authCookieName = 'token';
 
 // The scores and users are saved in memory and disappear whenever the service is restarted.
 let users = [];
-
-
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
-
 // JSON body parsing using built-in middleware
 app.use(express.json());
-
 // Use the cookie parser middleware for tracking authentication tokens
 app.use(cookieParser());
-
 // Serve up the front-end static content hosting
 app.use(express.static('public'));
-
 // Router for service endpoints
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
