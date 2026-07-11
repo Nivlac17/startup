b6e66ab8a79aca33078a56492985aacb385227b6
Calvin Merrell <calvinm7@byu.edu>
2026-07-10 21:53:23 -0600

    jsx files created for each page, functional app.css

---
+++ src/draw/draw.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Draw() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>draw displayed here</div>
+    </main>
+  );
+}
---
+++ src/navigation/navigation.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Navigation() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>navigation displayed here</div>
+    </main>
+  );
+}
