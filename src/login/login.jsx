9956b26b8ccefed5ea46db75b6a6373a22f3c0d1
Calvin Merrell <calvinm7@byu.edu>
2026-07-10 22:18:22 -0600

    functional navigation page and login page in react jsx

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -4,8 +4,20 @@ import './login.css';
 
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
+    <main>
+      <h1>Welcome to Lines of Light</h1>
+      <form method="get" action="draw.html">
+        <div>
+          <span>@</span>
+          <input type="text" placeholder="your@email.com" />
+        </div>
+        <div>
+          <span>🔒</span>
+          <input type="password" placeholder="password" />
+        </div>
+        <button type="submit">Login</button>
+        <button type="submit">Register</button>
+      </form>
     </main>
   );
 }
--- src/navigation/navigation.jsx
+++ src/navigation/navigation.jsx
@@ -1,11 +1,65 @@
 import React from 'react';
 import './navigation.css';
 
-
 export function Navigation() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>navigation displayed here</div>
+    <main>
+      <h2>Select an art piece or draw new art!</h2>
+
+      <div className="art-card">
+        <div
+          style={{
+            border: '2px solid #000',
+            padding: '12px',
+            marginBottom: '10px',
+          }}
+        >
+          <p>User</p>
+          <button type="button">Art Title</button>
+          <button type="button">Art Title</button>
+          <button type="button">Art Title</button>
+          <button type="button">Art Title</button>
+        </div>
+
+        <div
+          style={{
+            border: '2px solid #000',
+            padding: '12px',
+            marginBottom: '10px',
+          }}
+        >
+          <p>User</p>
+          <button type="button">Art Title</button>
+          <button type="button">Art Title</button>
+          <button type="button">Art Title</button>
+          <button type="button">Art Title</button>
+        </div>
+
+        <div
+          style={{
+            border: '2px solid #000',
+            padding: '12px',
+            marginBottom: '10px',
+          }}
+        >
+          <p>User</p>
+          <button type="button">Art Title</button>
+          <button type="button">Art Title</button>
+          <button type="button">Art Title</button>
+          <button type="button">Art Title</button>
+        </div>
+
+        <div
+          style={{
+            border: '2px solid #000',
+            padding: '12px',
+          }}
+        >
+          <button type="button">+</button>
+          <span> New Art: </span>
+          <input type="text" placeholder="Title" />
+        </div>
+      </div>
     </main>
   );
 }
