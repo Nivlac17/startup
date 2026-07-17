63b59e5a0cc0bc6a2682a9413d4b074db49f63ca
Calvin Merrell <calvinm7@byu.edu>
2026-07-16 21:08:40 -0600

    quote page using 3rd party api implemented

--- src/quote/quote.jsx
+++ src/quote/quote.jsx
@@ -1,27 +1,64 @@
-import React from 'react';
+import React from "react";
 import './quote.css';
 
-
 export function Quote() {
-  return (
-    <main>
-      <div id="picture" class="picture-box"><img width="400px" src="temple.jpg" alt="random" /></div>
+  const [quote, setQuote] = React.useState('Loading...');
+  const [quoteAuthor, setQuoteAuthor] = React.useState('unknown');
 
 
-      <div id="quote">
-        <h1>Inspiration</h1>
-         <div id="quote-box">
-            <p>
-                Sometimes the things that may or may not be true are the things a man needs to believe in the most. 
-                That people are basically good; that honor, courage, and virtue mean everything; that power and money, 
-                money and power mean nothing; that good always triumphs over evil; and I want you to remember this, that love... 
-                true love never dies. You remember that, boy. You remember that. Doesn't matter if it's true or not. You see, a man 
-                should believe in those things, because those are the things worth believing in.
-            </p>
-         </div>
-        <h3>-Second Hand Lions</h3>
-      </div>
+
+    React.useEffect(() => {
+
+    fetch('https://quote.cs260.click')
+    .then((response) => response.json())
+    .then((jsonResponse) => {
+        console.log(jsonResponse);
+        setQuote(jsonResponse.quote);
+        setQuoteAuthor(jsonResponse.author);
+    });
+     }, []);
+
+return (
+  
+     <main className="container-fluid ">
+        <div id="picture" class="picture-box"><img width="400px" src="temple.jpg" alt="random" /></div>
+
+      <div>
+      <h1>Inspiration</h1>
+      <div id="quote" className="quote-box">
+          <p className='quote'>{quote}</p>
+          <p className='author'>{quoteAuthor}</p>
+        </div>
+    </div>
     </main>
-
-  );
+);
 }
+
+
+// import React from 'react';
+// import './quote.css';
+
+
+// export function Quote() {
+//   return (
+//     <main>
+//       <div id="picture" class="picture-box"><img width="400px" src="temple.jpg" alt="random" /></div>
+
+
+//       <div id="quote">
+//         <h1>Inspiration</h1>
+//          <div id="quote-box">
+//             <p>
+//                 Sometimes the things that may or may not be true are the things a man needs to believe in the most. 
+//                 That people are basically good; that honor, courage, and virtue mean everything; that power and money, 
+//                 money and power mean nothing; that good always triumphs over evil; and I want you to remember this, that love... 
+//                 true love never dies. You remember that, boy. You remember that. Doesn't matter if it's true or not. You see, a man 
+//                 should believe in those things, because those are the things worth believing in.
+//             </p>
+//          </div>
+//         <h3>-Second Hand Lions</h3>
+//       </div>
+//     </main>
+
+//   );
+// }
