6010297a33ab12711f324a1e576645ca84c97d4e
Calvin Merrell <calvinm7@byu.edu>
2026-07-23 19:23:31 -0600

    authenticated.css file replaced and removed, login.css is updated as well

--- src/login/unauthenticated.jsx
+++ src/login/unauthenticated.jsx
@@ -66,11 +66,9 @@ export function Unauthenticated(props) {
     <>
       <div>
         <div className='input-group mb-3'>
-          {/* <span className='input-group-text'>@</span> */}
           <input className='form-control' type='text' value={userName} onChange={(e) => setUserName(e.target.value)} placeholder='your@email.com' />
         </div>
         <div className='input-group mb-3'>
-          {/* <span className='input-group-text'>🔒</span> */}
           <input className='form-control' type='password' onChange={(e) => setPassword(e.target.value)} placeholder='password' />
         </div>
         <Button className='ms-4' variant='primary' onClick={() => loginUser()} disabled={!userName || !password}>
